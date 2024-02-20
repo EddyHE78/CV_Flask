84d9a0e5ba36544cbaaf9eb908b23d89a3cb1e92
ElStaar <149469148+EddyHE78@users.noreply.github.com>
2024-02-20 12:13:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,32 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask,render_template
 
-app = Flask(__name__)  # Création de l'application Flask
+app = Flask(__name__) #creating flask app name
 
 @app.route('/')
 def home():
+    return render_template("index.html")
+
+@app.route('/resume_1')
+def resume_1():
     return render_template("resume_1.html")
 
-@app.route('/update_resume', methods=['POST'])
-def update_resume():
-    if request.method == 'POST':
-        # Récupérer les données du formulaire
-        name = request.form['name']
-        dob = request.form['dob']
-        email = request.form['email']
-        # Récupérer d'autres champs de formulaire de la même manière
-        
-        # Mettre à jour les données dans votre application (par exemple, dans une base de données)
-        # Ici, nous ne faisons que renvoyer les données pour l'exemple
-        updated_data = {
-            'name': name,
-            'dob': dob,
-            'email': email,
-            # Ajouter d'autres champs mis à jour de la même manière
-        }
-        
-        # Rediriger vers la page d'accueil avec un message de confirmation ou afficher un message sur la page
-        return render_template("resume_1.html", updated_data=updated_data)
+@app.route('/resume_2')
+def resume_2():
+    return render_template("resume_2.html")
 
-if __name__ == "__main__":
+@app.route('/resume_template')
+def resume_template():
+    return render_template("resume_template.html")
+
+if(__name__ == "__main__"):
     app.run()
-
