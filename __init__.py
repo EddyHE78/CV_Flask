b4d776536224c5c2304b67b03aff20ffa58a186e
ElStaar <149469148+EddyHE78@users.noreply.github.com>
2024-02-20 11:07:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app = Flask(__name__) #creating flask app name
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("resume_1.html")  # Modifier ici pour afficher resume_1.html
 
 @app.route('/resume_1')
 def resume_1():
@@ -18,5 +18,5 @@ def resume_2():
 def resume_template():
     return render_template("resume_template.html")
 
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     app.run()
