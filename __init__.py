91e9dbc1a2b6c7da4901da3ecda6ded83658b89f
ElStaar <149469148+EddyHE78@users.noreply.github.com>
2024-02-20 11:25:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,10 @@
+from flask import Flask,render_template
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
+app = Flask(__name__) #creating flask app name
 
 @app.route('/')
 def home():
-    # Vous pouvez charger main.yml ici pour afficher les liens
-    links = load_links_from_yaml("main.yml")
-    return render_template("index.html", links=links)
+    return render_template("index.html")
 
 @app.route('/resume_1')
 def resume_1():
@@ -21,14 +18,5 @@ def resume_2():
 def resume_template():
     return render_template("resume_template.html")
 
-def load_links_from_yaml(yaml_file):
-    # Fonction pour charger les liens depuis un fichier YAML
-    # Ici, vous pouvez utiliser une bibliothèque comme PyYAML pour charger le fichier YAML
-    # et extraire les liens
-    # Dans cet exemple, nous simulons simplement le chargement de liens statiques
-    with open(yaml_file, 'r') as file:
-        links_data = yaml.load(file, Loader=yaml.FullLoader)
-    return links_data['links']
-
-if __name__ == "__main__":
+if(__name__ == "__main__"):
     app.run()
